6f29569202978dc8252dcaf4b1f775952d466225
yuchiu54 <ycwaw15@gmail.com>
2024-02-05 22:04:43 +0800

    implement environment variables

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express")
+require("dotenv").config()
 server = express()
 
+function check_ip(ip) {
+    if (ip == process.env.IP_ONE) return true;
+    if (ip == process.env.IP_TWO) return true;
+    if (ip == process.env.IP_THREE) return true;
+    return false;
+}
+
 server.get("/", (req, res) => {
-    console.log("[ log ] visited");
-    res.send("Request passes WAF")
+    var ip = req.ip.split(":").pop()
+    if (check_ip(ip)) {
+        console.log("[ log ] visited");
+        res.send("Request passes WAF");
+    } else {
+        console.log("Unknown ip tend to connect: ", req.ip)
+        res.send("Forbidden");
+    }
 })
 
 server.listen(3000, () => {
